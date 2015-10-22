bd785e8ff22e9c2d674105abff691ec6a5532355
limeoats <admin@limeoats.com>
2015-10-22 15:34:44 -0400

    problem 13

--- main.cc
+++ main.cc
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 
-#include "problem12.h"
+#include "problem13.h"
 
 int main(int argc, const char* argv[]) {
-    std::cout << "Answer: " << problem12::getAnswer() << std::endl;
+    std::cout << "Answer: " << problem13::getAnswer() << std::endl;
 }
