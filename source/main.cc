425acf4031444aa073cb5556b0dc01798d62f53b
limeoats <admin@limeoats.com>
2015-11-09 19:48:49 -0500

    solved problem 18

--- source/main.cc
+++ source/main.cc
@@ -1,8 +1,8 @@
 #include <iostream>
 
-#include "problem16.h"
+#include "problem18.h"
 
 int main(int argc, const char* argv[]) {
-    std::cout << "Answer: " << problem16::getAnswer() << std::endl;
+    std::cout << "Answer: " << problem18::getAnswer() << std::endl;
 
 }
