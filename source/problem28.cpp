bc8aba7fc730ed04c9fa517ea01b8f68540948b1
Limeoats <admin@limeoats.com>
2015-12-02 17:32:18 -0500

    improved problem 28

--- source/problem28.cpp
+++ source/problem28.cpp
@@ -1,15 +1,30 @@
+/* Problem 28: Number spiral diagonals
+ * By: Mark Guerra
+ * 11/23/2015
+ */
+
 #include "problem28.h"
 #include <cmath>
 
-//n^2 + 1 - down left
-//n^2 - (n-1) - down right
-//n^2 + (n+1) - up left
-//odd n^2 - up right
+/*
+ * Start [total] at 1.
+ * There are four separate patterns for the diagonals:
+ * Down left: n^2 + 1
+ * Down right: n^2 - (n-1)
+ * Up left: n^2 + (n+1)
+ * Up right: odd n^2
+ * I combined each of the first 3 polynomials into:
+ * 3n^2 + 1, or 3(n^2 + 1)
+ * I then loop through each number from 2 to 1001.
+ * If it's an odd number, use formula n^2
+ * If it's an even number, use the combined formula 3(n^2 + 1)
+ * Add each result to [total] and return it;
+ */
 
-long problem28::getAnswer() {
-    int total = 1;
-    for (int i = 2; i < 1002; i++) {
-        total += (i & 1 == 1) ? pow(i, 2) : (3 + (pow(i, 2) * 3));
-    }
-    return total;
+unsigned int problem28::getAnswer() {
+    return ([](unsigned int total)->unsigned int {
+        for (int i = 2; i < 1002; i++)
+            total += (i & 1 == 1) ? pow(i, 2) : 3 * (pow(i, 2) + 1);
+        return total;
+    }(1));
 }
