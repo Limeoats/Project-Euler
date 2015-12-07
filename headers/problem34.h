b9d36a83ba50af778bbbd67b02affa849ce0d436
Limeoats <admin@limeoats.com>
2015-12-07 09:18:00 -0500

    solved problem 34

---
+++ headers/problem34.h
@@ -0,0 +1,16 @@
+/* Problem 34: Digit factorials
+ * By: Mark Guerra
+ * 12/7/2015
+ */
+
+#ifndef PROBLEM_34_H
+#define PROBLEM_34_H
+
+namespace problem34 {
+    int factorial(int n);
+    int getAnswer();
+}
+
+#endif
+
+
---
+++ source/problem34.cpp
@@ -0,0 +1,45 @@
+/* Problem 34: Digit factorials
+ * By: Mark Guerra
+ * 12/7/2015
+ */
+
+#include "problem34.h"
+
+#include <vector>
+#include <algorithm>
+
+/*
+ * Loop through every number from 3 to (9! * 4).
+ * Add up the sums of the factorials each digit. If that sum
+ * equals the original number, add it to a list. Return
+ * the sum of each item in the list.
+ */
+
+int problem34::factorial(int n) {
+    int x = 1;
+    for (int i = 1; i <= n; ++i) {
+        x *= i;
+    }
+    return x;
+}
+
+int problem34::getAnswer() {
+    std::vector<int> nums;
+    int sum = 0;
+    for (int i = 3; i < 1451520; i++) {
+        sum = 0;
+        int a = i;
+        while (true) {
+            sum += factorial(a % 10);
+            a /= 10;
+            if (a == 0) {
+                break;
+            }
+        }
+        if (sum == i) {
+            nums.push_back(i);
+        }
+    }
+    return std::accumulate(nums.begin(), nums.end(), 0);
+}
+
