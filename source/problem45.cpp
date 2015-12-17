2ce3ed422e6eb94d9b811564fc887e17eb10c856
Limeoats <admin@limeoats.com>
2015-12-17 17:31:34 -0500

    documentation

--- source/problem45.cpp
+++ source/problem45.cpp
@@ -7,6 +7,14 @@
 
 #include <cmath>
 
+/*
+ * Start [n] at 286, which is the index after the previous
+ * triangle number given in the problem. Loop around, incrementing
+ * [n] by 1 each time. Calculate the hexagonal number at that
+ * index. Check if that hexagonal number is also pentagonal and
+ * a triangle number. If both are true, return it and print it.
+ */
+
 bool problem45::isPentagonal(int x) {
     double t = (sqrt(1.0 + 24.0 * x) + 1.0) / 6.0;
     return  t == int(t);
