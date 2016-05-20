0b975c5c5304956b09e7fa1e26195dee0ab70aa1
Mark Guerra <admin@limeoats.com>
2016-05-20 13:47:40 -0400

    started problem 51

---
+++ src/problem051.cpp
@@ -0,0 +1,21 @@
+/*
+ * problem051.cpp
+ * By: Mark Guerra
+ * 5/20/2016
+*/
+
+#include "problem051.h"
+#include "utils.h"
+
+#include <iostream>
+#include <vector>
+
+int problem051::getAnswer() {
+    std::string num = "5700";
+    while (true) {
+        int familySize = 0;
+        for (int i = 1; i < num.length(); ++i) {
+
+        }
+    }
+}
