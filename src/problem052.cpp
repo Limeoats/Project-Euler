07cd3c7a7115b7a86d4acd330835d8f6e2901ce8
Limeoats <admin@limeoats.com>
2016-09-06 08:22:01 -0400

    solved problem 52

---
+++ src/problem052.cpp
@@ -0,0 +1,35 @@
+/*
+ * problem052.cpp
+ * By: Mark Guerra
+ * 9/6/2016
+*/
+
+#include <algorithm>
+
+#include "problem052.h"
+
+/*
+ * With [a] starting at 1, begin a loop. First, double [a] to get
+ * the first number to check permutations with. Next, start another loop
+ * where a number [i] goes up to 6, each time multiplying [a] by [i]. Then,
+ * check if the product is a permutation of 2*[a]. If it is, keep going.
+ * If the loop makes it up to 6*[a] and it is a permutation of the original 2*[a],
+ * return [a] as the lowest number and print it.
+ */
+
+int problem052::getAnswer() {
+    int a = 1;
+    while (true) {
+        std::string original = std::to_string(2 * a);
+        for (int i = 3; i < 7; ++i) {
+            std::string next = std::to_string(i * a);
+            if (!std::is_permutation(original.begin(), original.end(), next.begin())) {
+                break;
+            }
+            if (i == 6) {
+                return std::stoi(original) / 2;
+            }
+        }
+        ++a;
+    }
+}
