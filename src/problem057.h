37a592a90ac04d8930a6c8ae44944fb1f55f8cd9
Limeoats <admin@limeoats.com>
2016-09-13 22:23:38 -0400

    problem 57 finished

---
+++ src/problem057.h
@@ -0,0 +1,14 @@
+/*
+ * Problem 57: Square root convergents
+ * By: Mark Guerra
+ * 9/13/2016
+*/
+
+#ifndef PROJECT_EULER_PROBLEM057_H
+#define PROJECT_EULER_PROBLEM057_H
+
+namespace problem057 {
+    int getAnswer();
+}
+
+#endif
