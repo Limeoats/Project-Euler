3af6610f9771bd5b5c478694acec8fb247a46461
cw1650 <admin@limeoats.com>
2017-05-02 16:24:21 -0400

    problem 82 done

--- src/problem082.cpp
+++ src/problem082.cpp
@@ -29,22 +29,23 @@ std::string Problem082::getAnswer() {
     for (auto i = 0; i < numbers.size(); ++i) {
         tmp[i] = numbers[i][numbers.size() - 1];
     }
-    //I IS COLUMN
-    //J IS ROW
     for (int i = numbers.size() - 2; i >= 0; --i) {
         //Decide if it's better to go directly right or down-right
-        tmp[0] = std::min(numbers[0][i] + numbers[1][i], numbers[0][i] + numbers[0][i + 1]);
+        tmp[0] = std::min(numbers[0][i] + numbers[1][i] + tmp[1], tmp[0] + numbers[0][i]);
         for (int j = 1; j < numbers.size() - 1; ++j) {
             //Loop through the rows in column [i]
             //Check if tmp[j - 1] has its minimum value by going right or up-right (look up)
             if (tmp[j - 1] != numbers[j][i] + numbers[j - 1][i] + tmp[j]) {
+                //Set tmp[j] to the min of right, up-right, or down-right
                 tmp[j] = std::min(tmp[j - 1] + numbers[j][i], std::min(tmp[j] + numbers[j][i],
                                                                        numbers[j][i] + numbers[j + 1][i] + tmp[j + 1]));
             }
             else {
+                //Set tmp[j] to the min of right or down-right
                 tmp[j] = std::min(tmp[j] + numbers[j][i], numbers[j][i] + numbers[j + 1][i] + tmp[j + 1]);
             }
         }
+        //Check if right or up-right is less
         tmp[numbers.size() - 1] = std::min(tmp[numbers.size() - 1] + numbers[numbers.size() - 1][i],
                                            tmp[numbers.size() - 1] + numbers[numbers.size() - 1][i]);
     }
