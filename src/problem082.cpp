18728515ee9e9380942a6323eb9fee28d87296e9
cw1650 <admin@limeoats.com>
2017-05-03 09:05:09 -0400

    update problem 82

--- src/problem082.cpp
+++ src/problem082.cpp
@@ -16,19 +16,20 @@ std::string Problem082::getAnswer() {
         lines.push_back(line);
     }
     in.close();
-    std::vector<std::vector<int>> numbers(lines.size());
+    std::vector<std::vector<long>> numbers(lines.size());
     for (auto i = 0; i < lines.size(); ++i) {
         auto t = utils::split(lines[i], ',');
-        std::vector<int> c;
+        std::vector<long> c;
         for (auto x : t) {
             c.push_back(atoi(x.c_str()));
         }
         numbers[i] = c;
     }
-    std::vector<int> tmp(numbers.size());
+    std::vector<long> tmp(numbers.size());
     for (auto i = 0; i < numbers.size(); ++i) {
         tmp[i] = numbers[i][numbers.size() - 1];
     }
+    //Start at the second column from the right
     for (int i = numbers.size() - 2; i >= 0; --i) {
         //Decide if it's better to go directly right or down-right
         tmp[0] = std::min(numbers[0][i] + numbers[1][i] + tmp[1], tmp[0] + numbers[0][i]);
@@ -46,8 +47,8 @@ std::string Problem082::getAnswer() {
             }
         }
         //Check if right or up-right is less
-        tmp[numbers.size() - 1] = std::min(tmp[numbers.size() - 1] + numbers[numbers.size() - 1][i],
-                                           tmp[numbers.size() - 1] + numbers[numbers.size() - 1][i]);
+        int t = numbers.size() - 1;
+        tmp[t] = std::min(tmp[t - 1] + numbers[t][i], tmp[t] + numbers[t][i]);
     }
     return std::to_string(*std::min_element(std::begin(tmp), std::end(tmp)));
 }
