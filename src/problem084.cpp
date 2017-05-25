29653c328f38a8a39fdafa7a226f8ab44873fd13
Mark Guerra <admin@limeoats.com>
2017-05-25 18:14:05 -0400

    finish problem 84

--- src/problem084.cpp
+++ src/problem084.cpp
@@ -9,6 +9,17 @@
 #include <algorithm>
 #include "main.h"
 
+/*
+ * I decided to simulate 5,000,000 turns of Monopoly to solve this problem.
+ * Two dice are rolled, the [location] is moved accordingly, and if
+ * community chest or chance are landed on, the shuffled cards
+ * are looked at and a new location is determined. All jail logic
+ * is also implemented. The final location each turn is stored
+ * in a map [counts]. When the simulation is over, I check for the
+ * three highest values in [counts], pull out the key, and
+ * append it to a string. The string is then returned and printed.
+ */
+
 void Problem084::chance(std::vector<int> &cards, int &location) {
     if (cards[0] == 1) location = 0;
     else if (cards[0] == 2) location = 10;
@@ -16,8 +27,8 @@ void Problem084::chance(std::vector<int> &cards, int &location) {
     else if (cards[0] == 4) location = 24;
     else if (cards[0] == 5) location = 39;
     else if (cards[0] == 6) location = 5;
-    else if (cards[0] == 7 || cards[0] == 8) location = (location < 5 ? 5 : location < 15 ? 15 : location < 25 ? 25 : 35);
-    else if (cards[0] == 9) location = location < 12 ? 12 : 28;
+    else if (cards[0] == 7 || cards[0] == 8) location = (location < 5 || location > 35 ? 5 : location < 15 ? 15 : location < 25 ? 25 : 35);
+    else if (cards[0] == 9) location = location < 12 || location > 28 ? 12 : 28;
     else if (cards[0] == 10) location -= 3;
     std::rotate(std::begin(cards), std::begin(cards) + 1, std::end(cards));
 }
@@ -30,7 +41,7 @@ void Problem084::community_chest(std::vector<int> &cards, int &location) {
 
 std::string Problem084::getAnswer() {
     srand(static_cast<unsigned int>(time(nullptr)));
-    const int DICE_SIDES = 6;
+    const int DICE_SIDES = 4;
     
     std::map<int, int> counts;
     int location = 0;
@@ -41,10 +52,11 @@ std::string Problem084::getAnswer() {
     std::random_shuffle(chance_cards.begin(), chance_cards.end());
     std::random_shuffle(community_chest_cards.begin(), community_chest_cards.end());
     
-    for (int turns = 0; turns < 10000000; ++turns) {
+    for (int turns = 0; turns < 5000000; ++turns) {
         int r1 = rand() % DICE_SIDES + 1;
         int r2 = rand() % DICE_SIDES + 1;
         if (r1 == r2) ++doubles_count;
+        else doubles_count = 0;
         bool just_jailed = false;
         if (doubles_count == 3) {
             location = 10;
@@ -60,21 +72,16 @@ std::string Problem084::getAnswer() {
         }
         if (location == 2 || location == 17 || location == 33) community_chest(community_chest_cards, location);
         else if (location == 7 || location == 22 || location == 36) chance(chance_cards, location);
-        else if (location == 30) location = 10; //Go to jail
-        
+        else if (location == 30) location = 10;
         counts[location]++;
     }
-    
     std::string result = "";
     for (int i = 0; i < 3; ++i) {
         auto pr = std::max_element(counts.begin(), counts.end(), [](auto &p1, auto &p2) {
             return p1.second < p2.second;
         });
-        
         result += (pr->first < 10 ? "0" : "") + std::to_string(pr->first);
-        std::cout << "Card: " << (pr->first < 10 ? "0" : "") + std::to_string(pr->first) << std::endl;
         counts.erase(pr);
     }
-    
     return result;
 }
